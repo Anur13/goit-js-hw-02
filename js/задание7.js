e17c20c4ff06a4ab3fb7ed7f2e7c3d9a3e76a90b
Anur13 <akrupskyi13@gmail.com>
2020-12-03 13:17:07 +0200

    Update задание7.js

--- js/задание7.js
+++ js/задание7.js
@@ -23,19 +23,10 @@ const addLogin = function (allLogins, login) {
         logins.push(login)
         return "Логин успешно добавлен!"
     }
+    return "Логин не добавлен"
 }
 
-// const addLogin = function (allLogins, login) {
-//     if (isLoginValid(login) === true) {
-//     }
-//     return
-//     if (isLoginUnique(allLogins, login) === true) {
-//         logins.push(login)
-//         return "Логин успешно добавлен!"
-//     }
-// }
-
-// console.log(addLogin(logins, "Ajax")) // 'Логин успешно добавлен!'
-// console.log(addLogin(logins, "Aj4x1sBozz")) // 'Такой логин уже используется!'
+console.log(addLogin(logins, "Ajax")) // 'Логин успешно добавлен!'
+console.log(addLogin(logins, "Aj4x1sBozz")) // 'Такой логин уже используется!'
 console.log(addLogin(logins, "Zod")) // 'Ошибка! Логин должен быть от 4 до 16 символов'
-// console.log(addLogin(logins, "jqueryisextremelyfast")) // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, "jqueryisextremelyfast")) // 'Ошибка! Логин должен быть от 4 до 16 символов'
