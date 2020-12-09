261f7706d723ea9a7b8b6a353bb74515e5664d79
Anur13 <akrupskyi13@gmail.com>
2020-12-09 12:46:14 +0200

    2

--- js/task3.js
+++ js/task3.js
@@ -1,19 +1,25 @@
 // Вариант 1
 
-const findLongestWord = function (string) {
-    const words = string.split(" ")
-    let longestWord = ` `
-    for (let word of words) {
-        if (word.length > longestWord.length) {
-            longestWord = word
-        }
-    }
-    return longestWord
-}
+// const findLongestWords = function (string) {
+//     const words = string.split(" ")
+//     let longestWord = ` `
+//     for (let word of words) {
+//         if (word.length > longestWord.length) {
+//             longestWord = word
+//         }
+//     }
+//     return longestWord
+// }
+
+// console.log(findLongestWords("The quick brown fox jumped over the lazy dog")) // 'jumped'
+
+// console.log(findLongestWords("Google do a roll")) // 'Google'
+
+// console.log(findLongestWords("May the force be with you")) // 'force'
 
 // Вариант 2
 
-const findLongestWords = function (string) {
+const findLongestWord = function (string) {
     const words = string.split(" ")
 
     words.sort(function (a, b) {
@@ -21,11 +27,6 @@ const findLongestWords = function (string) {
     })
     return words[0]
 }
-console.log(findLongestWords("The quick brown fox jumped over the lazy dog")) // 'jumped'
-
-console.log(findLongestWords("Google do a roll")) // 'Google'
-
-console.log(findLongestWords("May the force be with you")) // 'force'
 
 console.log(findLongestWord("The quick brown fox jumped over the lazy dog")) // 'jumped'
 
